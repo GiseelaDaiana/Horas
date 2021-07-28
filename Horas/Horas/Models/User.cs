9b50b94a35eada919fcef81f2fcfd936567e0884
Gisela Tamburro <gtamburro@huenei.com>
2021-07-27 22:28:35 -0300

    agrego Entidad Usuario

--- Horas/Horas/Models/User.cs
+++ Horas/Horas/Models/User.cs
@@ -7,6 +7,15 @@ namespace Horas.Models
 {
     public class User
     {
+       public static bool iniciarSecion (string email , string password)
+
+            var query = base.User.where(p => p.email == email && p.password == password)
+
+            var query1 = from p in base.User
+                         where p.email == email && p.password == password
+                         select p
+
+                         if(Queryable.count > 0)
 
     }
 }
